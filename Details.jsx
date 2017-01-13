33d23491142fb2a84e4fcfc8deb77548a3f65c7a
Mihika Mishra <mishra.mihika@gmail.com>
2017-01-14 00:09:58 +0530

    Update Details.jsx

--- Details.jsx
+++ Details.jsx
@@ -1,10 +1,15 @@
 
 var Details=React.createClass({ 
+ 
  getInitialState:function()
- { return(nam:’’,value:’’”,begin[“”,””,””],end[“”,””,””]);},
+ { 
+    return(name:’’,value:’’,begin[“”,””,””],end[“”,””,””],display:false);
+ },
 
  getName: function(val)
-{ this.setState({name:val)};},
+{ 
+    this.setState({name:val)};
+},
 
  getValue: function(val)
 { 
@@ -16,7 +21,12 @@ var Details=React.createClass({
 { this.setState({begin[ind]:val)};},
 
  setEndDate: function(val,ind)
-{ this.setState({end[ind]:val)};},
+{  
+    var dateOK=true;
+    this.setState({end[ind]:val)};
+    if(ind==2)
+       dateOK=this.validate();
+ },
 
 validateDate: function()
 {
@@ -32,8 +42,31 @@ if(begin[0]>end[0] && begin[2]==end[2] && begin[1]==end[1] )
      return false;
 else return true;
 },
+ 
+ displayReport: function()
+ { 
+    this.setState({display:true});
+ },
+  
 render: function()
 {
+ var disRep='';
+ if(this.state.display)
+    disRep=(<div>Name:{this.state.name}<br/>
+   <div class="card">
+  <div class="donut-chart chart1">
+    <div class="slice one"></div>
+    <div class="slice two"></div>
+    <div class="chart-center">
+      <span>{this.state.value}</span>
+    </div>
+  </div>
+</div>
+   <br/>
+   Begin Date:{this.state.begin[0]}/{this.state.begin[1]}/{this.state.begin[2]}<br/>
+   End date:{this.state.end[0]}/{this.state.end[1]}/{this.state.end[2]}
+       </div>);
+ 
  return(<div>
     <span>Enter Name
     <input type="text" value={this.state.name} onChange={this.getName} />
@@ -54,7 +87,9 @@ render: function()
     <input type="text" value={this.state.end[1]} onChange={this.setEndDate.bind(this,"1")} />
     <input type="text" value={this.state.end[2]} onChange={this.setEndDate.bind(this,"2")} />
     </span>
-    
+   
+   <button onClick={this.displayReport}>Report</button>
+   {disRep}
 </div>);
 }
 )};
